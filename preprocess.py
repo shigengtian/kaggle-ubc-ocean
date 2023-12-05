56ceb4bf724269c31deaa7277c33aa71dd2011c7
shigengtian <shigengtian@gmail.com>
2023-12-05 21:20:59 +0900

    Changes to be committed: 	modified:   preprocess.py 	new file:   train2.py

--- preprocess.py
+++ preprocess.py
@@ -45,35 +45,24 @@ from glob import glob
 #     return files, idxs
 
 
-def extract_image_tiles(img_path, size=2048):
+def extract_image_tiles(img_id, img_path, size=2048):
     img = cv2.imread(str(img_path))
     h, w = size, size  # Set the size of the tiles
     height, width = img.shape[:2]
-    print(img.shape)
-    
-    save_path = "tiles" 
+
+    save_path = "tiles"
 
     # Calculate the number of rows and columns
     rows = height // h
     cols = width // w
 
-    # Extract tiles from the image
-    # tiles = []
     for i in range(rows):
         for j in range(cols):
-            tile = img[i * h: (i + 1) * h, j * w: (j + 1) * w]
-            tile_filename = f"tile_{i}_{j}.jpg"
-            # print(123123)
-            # tile_path = os.path.join(save_path, tile_filename)
-            tile_path = f"tiles/{tile_filename}"
+            tile = img[i * h : (i + 1) * h, j * w : (j + 1) * w]
+            tile_filename = f"{img_id}_{i}_{j}.jpg"
+
+            tile_path = f"dataset/tiles/{tile_filename}"
             cv2.imwrite(tile_path, tile)
-            
-            # tiles.append(tile)
-
-    # return tiles
-
-    
-    
 
     # if not inds:
     #     # https://stackoverflow.com/a/47581978/4521646
@@ -133,6 +122,18 @@ def get_mask_df(mask_dir):
     return mask_files_df
 
 
+def get_train_img_df(train_img_dir):
+    img_files = list(train_img_dir.glob("*.png"))
+    print(f"mask_files: {len(img_files)}")
+    train_img_df = pd.DataFrame(img_files, columns=["img_file_paths"])
+
+    train_img_df["image_id"] = train_img_df["img_file_paths"].apply(
+        lambda x: str(x).split("/")[-1].split(".")[0]
+    )
+
+    return train_img_df
+
+
 if __name__ == "__main__":
     data_dir = Path("dataset")
     train_df = pd.read_csv(data_dir / "train.csv")
@@ -143,30 +144,53 @@ if __name__ == "__main__":
     data_dir = Path("dataset")
     mask_dir = data_dir / "masks"
     train_thumbnails_dir = data_dir / "train_thumbnails"
+    train_img_dir = data_dir / "train_images"
 
     train_thumbnails_files_df = get_thumbnails(train_thumbnails_dir)
     mask_files_df = get_mask_df(mask_dir)
+    train_img_df = get_train_img_df(train_img_dir)
 
-    train_thumbnails_files_df = train_thumbnails_files_df.merge(
-        mask_files_df, on="image_id", how="left"
-    ).dropna()
+    train_df = mask_files_df.merge(train_thumbnails_files_df, on="image_id", how="left")
+    train_df = train_df.merge(train_img_df, on="image_id", how="left")
+    print(len(train_df))
 
-    # print(train_thumbnails_files_df.head())
-    # extract_image_tiles("dataset/train_thumbnails/5251_thumbnail.png")
-    
-    
-    extract_image_tiles("dataset/train_images/5251.png")
-    
-    # for index, row in train_thumbnails_files_df.iterrows():
-    #     thumbnails_fils_path = row["train_thumbnails_file_paths"]
-    #     mask_file_path = row["mask_file_paths"]
-    #     print(thumbnails_fils_path, mask_file_path)
-    #     break
+    tile_size = 2048
+    save_path = "dataset/tiles"
 
-    # train_df = pd.read_csv(data_dir / "train.csv", dtype={"image_id": str})
+    for index, row in train_df.iterrows():
+        img_id = row["image_id"]
+        img_path = row["img_file_paths"]
+        mask_path = row["mask_file_paths"]
+        img = cv2.imread(str(img_path))
 
-    # train_thumbnails_files_df = train_thumbnails_files_df.merge(
-    #     train_df, on="image_id", how="left"
-    # )
+        height, width = img.shape[:2]
 
-    # print(train_thumbnails_files_df.head())
+        mask = cv2.imread(str(mask_path))
+        mask = mask[:, :, 2]
+        mask = (mask > 0).astype(int)
+
+        rows = height // tile_size
+        cols = width // tile_size
+
+        for i in range(rows):
+            for j in range(cols):
+                tile_mask = mask[
+                    i * tile_size : (i + 1) * tile_size,
+                    j * tile_size : (j + 1) * tile_size,
+                ]
+                true_percentage = tile_mask.sum() / tile_mask.size
+                print(tile_mask.size)
+                if true_percentage < 0.5:
+                    continue
+
+                tile = img[
+                    i * tile_size : (i + 1) * tile_size,
+                    j * tile_size : (j + 1) * tile_size,
+                ]
+
+                tile_filename = f"{img_id}_{i}_{j}.jpg"
+                tile_path = f"{save_path}/{tile_filename}"
+                cv2.imwrite(tile_path, tile)
+
+        # print(img.shape, mask.shape)
+        # break
