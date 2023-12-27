5c03f1a4d0c3da72709cc01790115c627c319604
shigengtian <shigengtian@gmail.com>
2023-12-27 21:26:14 +0900

    Changes to be committed: 	modified:   test.py

--- test.py
+++ test.py
@@ -21,54 +21,6 @@ from PIL import Image
 import shutil
 
 
-# def vips_read_image(image_path, longest_edge):
-#     """
-#     Read image using libvips
-
-#     Parameters
-#     ----------
-#     image_path: str
-#         Path of the image
-
-#     Returns
-#     -------
-#     image: numpy.ndarray of shape (height, width, 3)
-#         Image array
-#     """
-
-#     image_thumbnail = pyvips.Image.thumbnail(image_path, longest_edge)
-
-#     return np.ndarray(
-#         buffer=image_thumbnail.write_to_memory(),
-#         dtype=np.uint8,
-#         shape=[image_thumbnail.height, image_thumbnail.width, image_thumbnail.bands],
-#     )
-
-
-# def process_image(row, data_dir, target_path):
-#     tma = row["is_tma"]
-#     if not tma:
-#         img_path = data_dir / "train_images" / f"{row.image_id}.png"
-#         img = vips_read_image(str(img_path), longest_edge=5000)
-#         cv2.imwrite(str(target_path / f"{row.image_id}.png"), img)
-#     else:
-#         img_path = data_dir / "train_images" / f"{row.image_id}.png"
-#         img = cv2.imread(str(img_path))
-#         cv2.imwrite(str(target_path / f"{row.image_id}.png"), img)
-
-
-# def convert_rgb_to_labels(img_path: str, folder: str):
-#     name = os.path.basename(img_path)
-#     img = np.array(Image.open(img_path))
-#     # plt.imshow(img)
-#     bg = np.ones((img.shape[0], img.shape[1], 1)) * 128
-#     stack = np.concatenate((bg, img), axis=2)
-#     mask = np.argmax(stack, axis=2).astype(np.uint8)
-#     img_path = os.path.join(folder, name)
-#     Image.fromarray(mask).save(img_path)
-#     return img_path
-
-
 def process_image(row, data_dir, tile_2048_path, tile_2048_mask_path):
     tma = row["is_tma"]
     image_id = row["image_id"]
@@ -98,29 +50,14 @@ def process_image(row, data_dir, tile_2048_path, tile_2048_mask_path):
                 total_pixels = np.prod(binary_image.shape[:2])
                 black_area_ratio = black_pixels / total_pixels
 
-                # black_pixels = total_pixels - np.count_nonzero(binary_image)
-
-                # black_area_ratio = black_pixels / total_pixels
 
                 if black_area_ratio > 0.3:
                     # print(f"black_area_ratio: {black_area_ratio}")
                     continue
-                # black area ratio threshold end here
-
-                # white area ratio threshold start from here
-                # white_pixels = np.count_nonzero(binary_image == 255)
-                # white_area_ratio = white_pixels / total_pixels
-
-                # if white_area_ratio > 0.7:
-                #     print(f"white_area_ratio: {white_area_ratio}")
-                #     continue
-                # white area ratio threshold end here
 
                 mask_tile = mask.crop(
                     x, y, min(tile_size, width - x), min(tile_size, height - y)
                 ).numpy()
-
-                # mask_tile_tumor_mask =
 
                 tile_save_path = tile_2048_path / f"{image_id}_tile_{x}_{y}.png"
                 Image.fromarray(img_tile).resize(new_size, Image.LANCZOS).save(
